2db73c16122cc462e303c12a3b81d1e3b203d988
DIM <4ebotarov_dim@inbox.ru>
2024-05-01 22:23:39 +0300

    the second option
    with amendments and additions

--- first_main.cpp
+++ first_main.cpp
@@ -1,18 +1,23 @@
-# include <iostream>
+#include <iostream>
+#include <iomanip> // Для использования std::setw
+#include <limits> // Необходимо для std::numeric_limits
+int main() {
+    std::cout << "\t Hello multiply\n";
 
-int main(int argc, char** argv){
+    int number = 0;
+    std::cout << "Enter number: ";
+    std::cin >> number;
 
- printf("\t Hello multiply \n"); 
- 
- int number =0;
- std::cout<<"Enter number :";
- std::cin >> number;
- 
- for(int i=1;i<=10;++i) {
- 	std::cout<<number<< " * " << i << " = " << number*i<< std::endl;
- }	
-	
-	
- system("pause>0");
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << std::setw(2) << number << " * " << std::setw(2) << i << " = " << std::setw(4) << number * i << std::endl;
+    }
+
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очистка буфера ввода
+    std::cin.get(); // Ожидание нажатия Enter
+
+    return 0;
+
+
  return 0;	
 }
