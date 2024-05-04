4f6a4ce64ade98dd7e7af2dea415f32b08f648ef
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-04 10:27:37 +0300

    full output of the table with one more correction

--- thri_main.cpp
+++ thri_main.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
+
 int main() {
-	std::cout << "\t Hello multipy\n ";
-  
-   for( int i=1;i<=9;i++) {
-   	for( int j=1;j<=9;j++) {
-   		std::cout<<std::right<<std::setw(3)<<i*j<<"  ";
-	   }
-	   std::cout<<"\n";
-   }
-	return 0;
+    std::cout << "\tHello multiplyn\n\n";
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            std::cout << std::setw(4) << i * j;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
