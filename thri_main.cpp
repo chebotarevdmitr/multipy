2831997d0748704d73c54fb37304dd948749dd85
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-04 10:17:35 +0300

    full table output with correction

--- thri_main.cpp
+++ thri_main.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
-
+#include<iomanip>
 int main() {
 	std::cout << "\t Hello multipy\n ";
   
    for( int i=1;i<=9;i++) {
    	for( int j=1;j<=9;j++) {
-   		std::cout.width(4);
-   		std::cout<<i*j<<" ";
+   		std::cout<<std::right<<std::setw(3)<<i*j<<"  ";
 	   }
 	   std::cout<<"\n";
    }
